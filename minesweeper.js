82d961573af5aed0cd7b41a530bc72c7232158a8
Juwanxi <juwanrc@gmail.com>
2022-01-18 11:57:20 -0600

    minesweeper solution

--- minesweeper.js
+++ minesweeper.js
@@ -4,5 +4,50 @@
 //
 
 export const annotate = (input) => {
-  throw new Error('Remove this statement and implement this function');
-};
+
+if (input.length === 0)
+  return input;
+
+if (input[0].length === 0)
+  return input;
+
+const grid = input.map(str => [...str]);
+
+const row = grid.length;
+const col = grid[0].length;
+for (let i = 0; i < m; i++) {
+  for (let j = 0; j < n; j++) {
+    if (grid[i][j] == ' ') {
+      let mineCount = 0;
+      // top left
+      if (i > 0 && j > 0 && grid[i - 1][j - 1] === '*')
+        mineCount++;
+      // top middle
+      if (i > 0 && grid[i - 1][j] === '*')
+        mineCount++;
+      // top right
+      if (i > 0 && j < n - 1 && grid[i - 1][j + 1] === '*')
+        mineCount++;
+      // middle left
+      if (j > 0 && grid[i][j - 1] === '*')
+        mineCount++;
+      // middle right
+      if (j < n - 1 && grid[i][j + 1] === '*')
+        mineCount++;
+            
+      // bottom left
+      if (i < m - 1 && j > 0 && grid[i + 1][j - 1] === '*')
+        mineCount++;
+      // bottom middle
+      if (i < m - 1 && grid[i + 1][j] === '*')
+        mineCount++;
+      // botom right
+      if (i < m - 1 && j < n - 1 && grid[i + 1][j + 1] === '*')
+        mineCount++;
+      if (mineCount > 0)
+        grid[i][j] = mineCount;
+    }
+  }
+}
+return grid.map(arr => arr.join(''));
+}
